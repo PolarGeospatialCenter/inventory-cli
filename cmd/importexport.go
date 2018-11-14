b56a57a7f4e1c94a73f43070bfb54e876a98e3db
Andrew Zenk <azenk@umn.edu>
2018-11-14 11:30:51 -0600

    Implement import functionality

--- cmd/importexport.go
+++ cmd/importexport.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -11,6 +12,7 @@ import (
 )
 
 func init() {
+	rootCmd.AddCommand(cmdImport)
 	rootCmd.AddCommand(cmdExport)
 }
 
@@ -19,6 +21,76 @@ type InventoryBackup struct {
 	Nodes      []*types.Node
 	Networks   []*types.Network
 	Systems    []*types.System
+}
+
+var cmdImport = &cobra.Command{
+	Use:        "import filename",
+	ArgAliases: []string{"filename"},
+	Args:       cobra.MinimumNArgs(1),
+	Short:      "import objects from a backup",
+	Run: func(cmd *cobra.Command, args []string) {
+		api, err := apiConnect()
+		if err != nil {
+			log.Fatalf("unable to connect to api: %v", err)
+		}
+		backupData := &InventoryBackup{}
+
+		backupDataBytes, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			log.Fatalf("unable to open backup file: %v", err)
+		}
+
+		err = json.Unmarshal(backupDataBytes, backupData)
+		if err != nil {
+			log.Fatalf("unable to unmarshal backup data: %v", err)
+		}
+
+		for _, node := range backupData.Nodes {
+			n, err := api.Node().Get(node.ID())
+			if err != nil {
+				err = api.Node().Create(node)
+				if err != nil {
+					log.Fatalf("Unable to create node: %v", err)
+				}
+			} else if n.Timestamp() < node.Timestamp() {
+				err = api.Node().Update(node)
+				if err != nil {
+					log.Fatalf("Unable to update node: %v", err)
+				}
+			}
+		}
+
+		for _, system := range backupData.Systems {
+			n, err := api.System().Get(system.ID())
+			if err != nil {
+				err = api.System().Create(system)
+				if err != nil {
+					log.Fatalf("Unable to create system: %v", err)
+				}
+			} else if n.Timestamp() < system.Timestamp() {
+				err = api.System().Update(system)
+				if err != nil {
+					log.Fatalf("Unable to update system: %v", err)
+				}
+			}
+		}
+
+		for _, network := range backupData.Networks {
+			n, err := api.Network().Get(network.ID())
+			if err != nil {
+				err = api.Network().Create(network)
+				if err != nil {
+					log.Fatalf("Unable to create network: %v", err)
+				}
+			} else if n.Timestamp() < network.Timestamp() {
+				err = api.Network().Update(network)
+				if err != nil {
+					log.Fatalf("Unable to update network: %v", err)
+				}
+			}
+		}
+
+	},
 }
 
 var cmdExport = &cobra.Command{
