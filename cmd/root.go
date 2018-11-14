8ad46fc2202b155ff68123a96ea2c2ac0d41780d
Andrew Zenk <azenk@umn.edu>
2018-11-14 10:59:09 -0600

    Refactor to use common inventory api configuration function

--- cmd/root.go
+++ cmd/root.go
@@ -3,18 +3,12 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"path"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var (
-	cfgFile      string
-	apiBaseUrl   string
-	awsRegion    string
-	awsVaultRole string
+	inventoryCfgProfile string
 )
 
 var rootCmd = &cobra.Command{
@@ -34,41 +28,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.inventory-cli.yml)")
-	rootCmd.PersistentFlags().StringVarP(&apiBaseUrl, "api-url", "u", "", "base url for api calls")
-	rootCmd.PersistentFlags().StringVarP(&awsRegion, "aws-region", "r", "us-east-2", "aws region to use for authentication")
-	rootCmd.PersistentFlags().StringVar(&awsVaultRole, "aws-vault-role", "", "if set, attempt to obtain sts credentials from vault")
-	viper.BindPFlag("baseurl", rootCmd.PersistentFlags().Lookup("api-url"))
-	viper.BindPFlag("aws.region", rootCmd.PersistentFlags().Lookup("aws-region"))
-	viper.BindPFlag("aws.vault_role", rootCmd.PersistentFlags().Lookup("aws-vault-role"))
-	cmdNode.AddCommand(cmdNodeList)
-	cmdNode.AddCommand(cmdNodeInteractiveCreate)
-	cmdNode.AddCommand(cmdNodeDetectNetworks)
-	rootCmd.AddCommand(cmdNode)
-}
-
-func initConfig() {
-	// Don't forget to read config either from cfgFile or from home directory!
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(path.Join(home, ".inventory-cli"))
-		viper.AddConfigPath("/etc/inventory-cli")
-		viper.SetConfigName("inventory-cli")
-	}
-
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Can't read config:", err)
-		os.Exit(1)
-	}
+	rootCmd.PersistentFlags().StringVar(&inventoryCfgProfile, "profile", "", "inventory cli config profile name")
 }
