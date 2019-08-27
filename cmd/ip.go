15b6f41373c3970eecc68b4e4225aa51d4673949
Andrew Zenk <azenk@umn.edu>
2019-08-27 08:05:06 -0500

    Add commands to delete and convert IPs to static

--- cmd/ip.go
+++ cmd/ip.go
@@ -18,6 +18,8 @@ var cmdIp = &cobra.Command{
 
 func init() {
 	cmdIp.AddCommand(cmdIpShow)
+	cmdIp.AddCommand(cmdIpDelete)
+	cmdIp.AddCommand(cmdIpMakeStatic)
 	rootCmd.AddCommand(cmdIp)
 }
 
@@ -43,5 +45,65 @@ func ShowIP(_ *cobra.Command, args []string) {
 			log.Fatalf("Unable to get reservation for %s: %v", ip, err)
 		}
 		log.Print(reservation)
+	}
+}
+
+var cmdIpDelete = &cobra.Command{
+	Use:   "delete",
+	Short: "delete ip reservation",
+	Run:   DeleteIP,
+}
+
+func DeleteIP(_ *cobra.Command, args []string) {
+	apiClient, err := apiConnect()
+	if err != nil {
+		log.Fatalf("Unable to connect to api: %v", err)
+	}
+
+	for _, ipString := range args {
+		ip := net.ParseIP(ipString)
+		if ip == nil {
+			log.Fatalf("invalid IP: %s", ipString)
+		}
+		reservation, err := apiClient.IPAM().GetIPReservation(ip)
+		if err != nil {
+			log.Fatalf("Unable to get reservation for %s: %v", ip, err)
+		}
+
+		err = apiClient.IPAM().DeleteIPReservation(reservation)
+		if err != nil {
+			log.Fatalf("Unable to delete reservation for %s: %v", ip, err)
+		}
+	}
+}
+
+var cmdIpMakeStatic = &cobra.Command{
+	Use:   "make-static",
+	Short: "make ip reservation static",
+	Run:   MakeIPStatic,
+}
+
+func MakeIPStatic(_ *cobra.Command, args []string) {
+	apiClient, err := apiConnect()
+	if err != nil {
+		log.Fatalf("Unable to connect to api: %v", err)
+	}
+
+	for _, ipString := range args {
+		ip := net.ParseIP(ipString)
+		if ip == nil {
+			log.Fatalf("invalid IP: %s", ipString)
+		}
+		reservation, err := apiClient.IPAM().GetIPReservation(ip)
+		if err != nil {
+			log.Fatalf("Unable to get reservation for %s: %v", ip, err)
+		}
+
+		reservation.End = nil
+
+		_, err = apiClient.IPAM().UpdateIPReservation(reservation)
+		if err != nil {
+			log.Fatalf("Unable to make reservation static for %s: %v", ip, err)
+		}
 	}
 }
